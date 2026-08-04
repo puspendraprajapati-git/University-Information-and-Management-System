a199a4df536eb6c5b073e4ec8262e3c9114628b0
puspendra prajapati <puspendraprajapati.229@gmail.com>
2026-08-04 23:02:50 +0530

    update faculty management module

--- university-service/src/main/java/com/university/repository/FacultyRepository.java
+++ university-service/src/main/java/com/university/repository/FacultyRepository.java
@@ -2,8 +2,11 @@ package com.university.repository;
 
 import com.university.entity.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-    List<Faculty> findByDepartment_DeptId(Long deptId);
+    List<Faculty> findByDepartment_Id(Long deptId);
 }
